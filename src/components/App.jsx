96e7048c1b57c4e4f8ad846e4515d323e4ff780f
Osama Ahmed Qureshi <osamaxqureshi@outlook.com>
2023-12-11 23:52:37 +0500

    inline CSS Style check

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Login from "./Login";
 
 var isLoggedIn = false;
+const strikeThrough = { textDecoration: "line-through" };
 
 function App() {
   return <div className="container">{  isLoggedIn ?  <h1>Hello</h1> : <div>
-  <h1>Hello</h1>
+  <h1 style={isLoggedIn === false ? strikeThrough : null}>Hello</h1>
   <Login />
   </div>
   }
